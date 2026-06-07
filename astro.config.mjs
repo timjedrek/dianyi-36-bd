9a76d06008e3e3c53909ec9b975b999bce66c1d4
timjedrek <tim@timjedrek.com>
2026-06-07 15:41:45 -0500

    remove git pages and just use netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://astronaut.github.io',
-    base: '/dianyi-36-bd/',
+    // site: 'https://astronaut.github.io',
+    // base: '/dianyi-36-bd/',
 });
